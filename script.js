bbbe613c377eb16bf0851fefb79a2c5d93c7b945
RomanGaram <Romangaram@gmail.com>
2023-03-01 18:52:44 +0100

    working commit

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let firstnumber
-let secondnumber
+
+const SECOND_NUMBER = 0
 
 firstnumber = prompt("Please enter first number");
-secondnumber = prompt("Please enter second number");
+let secondnumber = prompt("Please enter second number");
+if ()
+
+
 alert (String("adding=") + (Number(firstnumber) + Number(secondnumber)));
 
 alert ("Subtraction=" + (Number(firstnumber) - Number(secondnumber)));
  
-alert (String("Division=") + Number(firstnumber) / Number(secondnumber));
+alert 'Division' = (Number(firstnumber) / Number(secondnumber));
 
 alert (String("Multiplication=") + Number(firstnumber) * Number(secondnumber));
