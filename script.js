aadb096c8af7ff46599ee1ad20240c9bb60da7e9
RomanGaram <Romangaram@gmail.com>
2023-03-01 13:32:23 +0100

    JS code for Lesson-21

---
+++ script.js
@@ -0,0 +1,12 @@
+let firstnumber
+let secondnumber
+
+firstnumber = prompt("Please enter first number");
+secondnumber = prompt("Please enter second number");
+alert (String("  adding=") +  (Number(firstnumber) + Number(secondnumber))
++ "  subtraction=" + 
+ (Number(firstnumber) - Number(secondnumber)) 
+ + "  division=" + 
+ (Number(firstnumber) / Number(secondnumber)) 
+ + "  multiplication=" +
+ (Number(firstnumber) * Number(secondnumber)));
