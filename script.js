173c0ae1fccbbeee811a2fc2d815b7728102ed33
RomanGaram <Romangaram@gmail.com>
2023-03-03 14:58:16 +0100

    Final commit to Lesson-22

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-let firstNumber
-let secondNumber
-let answer
-
 firstNumber = +prompt("Please enter first number", '');
 secondNumber = +prompt("Please enter second number", '');
 
-if ((firstNumber === '') || (secondNumber === '')) { 
+if ((firstNumber == '') || (secondNumber === '')) { 
     alert ('ERROR');   
 } 
 else {
 
         alert (String("adding=") + (Number(firstNumber) + Number(secondNumber)));
 
-    if (firstNumber >= secondNumber) {
-        alert (String("Subtraction=") + (Number(firstNumber) - Number(secondNumber)));
-    } else (firstNumber < secondNumber) {
-        confirm ('Are you sure you want to proceed with the operation?');
+
+    if (firstNumber < secondNumber) {
+       confirm ('Are you sure you want to proceed with the operation?');
+    } if (true) {
         alert (String("Subtraction=") + (Number(firstNumber) - Number(secondNumber)));
     }
 
-    if (secondNumber == 0) {
+    if (secondNumber === 0) {
         alert (String('You cannot divide by ZERO!'));
     }
     else if (secondNumber > 0) {
