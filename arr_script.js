ef9a797738b7f00418feca54be922c74f1bf42c4
RomanGaram <Romangaram@gmail.com>
2023-03-09 22:00:02 +0100

    a

---
+++ arr_script.js
@@ -0,0 +1,19 @@
+// let new_Arr = ['three', -5, 66, 'Falcon', 1991, 0, 'Owl', -166, 18, 5];
+// let onlyNumberArr = [];
+// let sum = 0
+
+
+
+// for(let i = 0; i < new_Arr.length; i++) {
+//     if (typeof new_Arr[i] !== 'number') {
+//         continue;
+//     }
+// }
+
+
+let drow_tree = [''];
+
+for (let i=0; i < 5; i++) {
+    console.log(drow_tree + '#');
+}
+console.log
